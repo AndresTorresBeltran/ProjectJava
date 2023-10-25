2d363e15c8f148f3fa04cf2b87ee08778f02d69a
AndresTorresBeltran <105888732+AndresTorresBeltran@users.noreply.github.com>
2023-10-24 22:43:09 -0500

    Testing first steps

---
+++ src/Teacher.java
@@ -0,0 +1,23 @@
+public class Teacher extends school{
+
+    private boolean fulltime;
+
+    public Teacher(int id, String name, boolean fulltime){
+        super(id, name);
+        this.fulltime=fulltime;
+
+    }
+    public double calcular_salario(){
+        int porcentaje = 0;
+        double total_salario=0;
+        int anios_exp=0;
+
+
+        return porcentaje;
+
+
+    }
+    System.out.println("HOla");
+
+
+}
