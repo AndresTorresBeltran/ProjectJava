0e07b9b02d8dcaaa5b03efceaf738c922ec5413c
AndresTorresBeltran <105888732+AndresTorresBeltran@users.noreply.github.com>
2023-10-29 01:47:15 -0500

    lots of mistakes

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,155 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        ArrayList Teacherslist = new ArrayList();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //Teachers
+        Teacher teacher1 = new Teacher(1, "Cayo Perez", true, 40, 2);
+        Teacherslist.add(teacher1);
+        Teacher teacher2 = new Teacher(2, "Joel Torres", true, 40, 3);
+        Teacherslist.add(teacher2);
+        Teacher teacher3 = new Teacher(3, "Javier Arellano", false, 14, 0);
+        Teacherslist.add(teacher3);
+        Teacher teacher4 = new Teacher(4, "Jorge gozo", false, 20, 0);
+        Teacherslist.add(teacher4);
+        //Students
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = hola testeando" + i);
+        ArrayList StudentsList = new ArrayList<>();
+        Student student1 = new Student(988, "Alfredo Espinoza", 23);
+        StudentsList.add(student1);
+        Student student2 = new Student(983, "Kevin Sote", 20);
+        StudentsList.add(student2);
+        Student student3 = new Student(788, "Zoyla Quebarre", 22);
+        StudentsList.add(student3);
+        Student student4 = new Student(430, "Deivid Lopez", 20);
+        StudentsList.add(student4);
+        Student student5 = new Student(838, "Melissa Torres", 19);
+        StudentsList.add(student5);
+        Student student6 = new Student(333, "Alfreo Marin", 22);
+        StudentsList.add(student6);
+        //Classes
+        ArrayList Clases = new ArrayList();
+        Clase Dibujo = new Clase("Dibujo 1", "Salon 104", teacher1);
+        Dibujo.addStudent(student1);
+        Dibujo.addStudent(student2);
+        Dibujo.addStudent(student6);
+        Clases.add(Dibujo);
+        Clase Python = new Clase("Python 2", "Salon 106", teacher4);
+        Python.addStudent(student2);
+        Python.addStudent(student3);
+        Python.addStudent(student4);
+        Clases.add(Python);
+        Clase Photo = new Clase("Photo", "Salon 201", teacher3);
+        Photo.addStudent(student3);
+        Photo.addStudent(student4);
+        Photo.addStudent(student5);
+        Clases.add(Photo);
+        Clase Java = new Clase("Java POO", "Salon 204", teacher1);
+        Java.addStudent(student3);
+        Java.addStudent(student5);
+        Java.addStudent(student6);
+        Clases.add(Java);
+
+
+        System.out.println("******** Welcome to the University System ********\n");
+        boolean correr = true;
+
+        //Uso un while para validar que siga corriendo el programa
+        while (correr) {
+            System.out.println("1. Teachers information");
+            System.out.println("2. Classes Info");
+            System.out.println("3. Add a new Student");
+            System.out.println("4. Add a new Class");
+            System.out.println("5. Search an student in a class"
+            );
+            System.out.println("6. " +
+                    "End program");
+
+
+            int Option = sc.nextInt();
+            switch (Option) {
+                // 1. Teachers Information
+                case (1):
+                    int i = 1;
+
+                    for (Object teacher : Teacherslist) {
+                        System.out.println("  " + i + ". " + " Teacher's Name" + teacher.getClass().getName() + "salary: " + Teacherslist.get(i).getClass());
+                        i++;
+                    }
+
+
+                    break;
+
+                // 2. Classes Information.
+                case (2):
+                    i = 1;
+                    //Printing class list
+                    System.out.println("Class List");
+                    for (i = 0; i < Clases.size(); i++) {
+                        System.out.println(i + ". " + Clases.get(i).getClass().getName() + "Student " + StudentsList.get(i).getClass().getName() + "Teachers: " + teacher.getName());
+
+                    }
+                    //Choosing class
+
+                    break;
+
+                // 3. Add new Student.
+                case (3):
+                    System.out.println("Add new student");
+                    //Student's Information
+                    System.out.println("Type Student's id:");
+                    int newStudentId = sc.nextInt();
+                    System.out.println("Type Student's name:");
+                    String newStudentName = sc.next();
+                    System.out.println("Type Student's age:");
+                    int newAge = sc.nextInt();
+                    Clase.addStudent(new Student(newStudentId, newStudentName, newAge));
+                    //Add to a class with a list of an existing classes
+
+
+                    break;
+
+
+                // 4. new Class.
+                case (4):
+                    System.out.println("Add new Class");
+
+                    //Student's Information
+                    System.out.println("class name");
+                    String newClassName = sc.next();
+                    System.out.println("classroom number with the format Salon 101");
+                    String newClassRoom = sc.next();
+
+
+                    Clase nuevaclase = new Clase(newClassName, newClassRoom, teacher1);
+                    nuevaclase.addStudent(student1);
+
+
+                    // 5. Search for a student's classes.
+                case (5):
+                    System.out.println("Students List:");
+                    for (i = 0; i < StudentsList.size(); i++) {
+                        System.out.println(" " + i + ". " + StudentsList.get(i).getClass() + " - id: 0" + StudentsList.get(i).getClass().getName());
+                        i++;
+                    }
+                    //Selecting student to show information and validate
+                    int numberStudentID = sc.nextInt();
+                    Student studentSelected = (Student) StudentsList.get(Dibujo.studentID);
+
+                    break;
+                // 6. Exit System.
+                case (6):
+                    System.out.println("Thanks for use this app!");
+                    correr = false;
+                    // wrong number
+                default:
+                    System.out.println("Write a valid option");
+            }
         }
     }
 }
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,19 +1,56 @@
 public class Teacher extends school{
+    private static final int PORCENTAJE1 = 100;
+    private static final int PORCENTAJE2 = 110;
 
     private boolean fulltime;
+    int hours;
 
-    public Teacher(int id, String name, boolean fulltime){
+    public boolean isFulltime() {
+
+        return fulltime;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    int years;
+
+    public Teacher(int id, String name, boolean fulltime, int hours, int years){
         super(id, name);
         this.fulltime=fulltime;
+        this.hours=hours;
+        this.years=years;
 
     }
     public double calcular_salario(){
         int porcentaje = 0;
+
         double total_salario=0;
         int anios_exp=0;
 
+        if (fulltime== true) {
+            porcentaje= PORCENTAJE1;
+            anios_exp=years;
+        }
 
-        return porcentaje;
+
+        else{
+            porcentaje= PORCENTAJE2;
+        }// payment depend of the weekly hours in my model.
+        total_salario= hours*total_salario*porcentaje/2;
+
+
+
+
+
+
+
+        return total_salario;
 
 
     }
